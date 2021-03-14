65a0e448626d1f268dc2061d165f2cd21dfb265a
man.nguyen <nguyentrongminhman95@gmail.com>
2021-03-14 17:53:48 +0700

    update

--- src/containers/SignUp/index.jsx
+++ src/containers/SignUp/index.jsx
@@ -2,7 +2,7 @@ import { Button, Card, Checkbox, Container, FormControl, FormControlLabel } from
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import { Link } from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss'
 
 const useStyles = makeStyles((theme) => ({
@@ -15,24 +15,60 @@ const useStyles = makeStyles((theme) => ({
 
 const SignUp = () => {
     const classes = useStyles();
+
+    const [name ,setName] = useState("")
+    const [email,setEmail] = useState("")
+    const [password,setPassWord] = useState("")
+    const [comfirmPass , setComfirmPass] = useState("")
+    const handleSignUp = (e) => {
+        e.preventDefault()
+        console.log(name,email,password,comfirmPass)
+    }
+
+
+
     return (
         <>
             <div className="sign-up">
                 <Container>
                     <h2 className="heading-login">Sign Up</h2>
                     <Card className="main">
-                        <form className={classes.root} noValidate autoComplete="off">
+                        <form className={classes.root} onSubmit = { handleSignUp }>
                             <FormControl fullWidth >
-                                <TextField id="name" label="Name" variant="outlined" value="" />
+                                <TextField 
+                                    id="name" label="Name"
+                                    variant="outlined" 
+                                    value={ name } 
+                                    onChange = { e => setName(e.target.value) } 
+                                />
                             </FormControl>
                             <FormControl fullWidth >
-                                <TextField id="email" label="Email" variant="outlined" value="" />
+                                <TextField 
+                                    id="email" 
+                                    label="Email" 
+                                    variant="outlined" 
+                                    value= { email }
+                                    onChange = { e => setEmail(e.target.value) } 
+                                />
                             </FormControl>
                             <FormControl fullWidth >
-                                <TextField id="password" label="password" type="password" value="" variant="outlined" />
+                                <TextField 
+                                    id="password" 
+                                    label="password" 
+                                    type="password" 
+                                    variant="outlined" 
+                                    value={ password }
+                                    onChange = { e => setPassWord(e.target.value) }
+                                />
                             </FormControl>
                             <FormControl fullWidth >
-                                <TextField id="cfmpassword" label="​confirm password" type="password" value="" variant="outlined" />
+                                <TextField 
+                                    id="cfmpassword" 
+                                    label="​confirm password" 
+                                    type="password" 
+                                    value={ comfirmPass }
+                                    onChange = { e => setComfirmPass(e.target.value) }
+                                    variant="outlined" />
                             </FormControl>
                             <FormControl>
                                 <FormControlLabel 
---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+var firebaseConfig = {
+    apiKey: "AIzaSyCmZRQcegXH7i1rtjy-pxgSV3kvMxusCtY",
+    authDomain: "auth-re-ech-app.firebaseapp.com",
+    projectId: "auth-re-ech-app",
+    storageBucket: "auth-re-ech-app.appspot.com",
+    messagingSenderId: "461094887941",
+    appId: "1:461094887941:web:33084349d6c0d6c1d8b26c",
+    measurementId: "G-FP4B13H22B"
+};
+const fire = firebase.initializeApp(firebaseConfig);
+export default fire
