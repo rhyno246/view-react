8e9a70ecb4944c9d598ce486c9671374ea3a4a58
Man Nguyen <nguyentrongminhman95@gmail.com>
2021-03-20 10:24:03 -0700

    test

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="foter">
             <div className="copy">
-                Copy right 2021
+                Copy right By Man Nguyen
             </div>
         </div>
     )
